7bb95c5d8b39260985703076df5603334df491a9
c4t4 <taesun4767@gmail.com>
2026-01-27 17:53:20 +0900

    feat: 메인 화면 구조 변경

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -9,6 +9,26 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'm.samkyoung.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'k.kakaocdn.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ssl.pstatic.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
